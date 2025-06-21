81b8bd492134c459924a944857fb2c0e62239367
ayaan <asm.ayyaan@gmail.com>
2025-06-21 00:26:59 -0700

    Working with Hero Module (Part -2)

--- database/migrations/2025_06_21_064241_create_hero_sections_table.php
+++ database/migrations/2025_06_21_064241_create_hero_sections_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('hero_sections', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
+            $table->string('title');
+            $table->string('subtitle');
+            
+            $table->string('counter_one')->nullable();
+            $table->string('counter_title_one')->nullable();
+
+            $table->string('counter_tow')->nullable();
+            $table->string('counter_title_two')->nullable();
+
+            $table->string('counter_three')->nullable();
+            $table->string('counter_title_three')->nullable();
+
+            $table->string('counter_four')->nullable();
+            $table->string('counter_title_four')->nullable();
+
             $table->timestamps();
         });
     }
--- resources/views/admin/hero/index.blade.php
+++ resources/views/admin/hero/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="card border-top-dark border-top-3 mb-3">
             <div class="card-header d-flex justify-content-between align-items-center">
-               <div> <b>Hero Section</b></div>
+                <div> <b>Hero Section</b></div>
                 {{-- <div class="card-header-actions">
                     <a class="btn btn-primary" href="">Create</a>
                 </div> --}}
@@ -12,7 +12,91 @@
 
 
             <div class="card-body text-dark">
-                
+                <form action="">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Image</label>
+                                <input type="file" class="form-control mb-2" name="image" id="image">
+                            </div>
+                        </div>
+                         <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Title</label>
+                                <input type="text" class="form-control mb-2" name="title" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Subtitle</label>
+                                <input type="text" class="form-control mb-2" name="subtitle" >
+                            </div>
+                        </div>
+
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter one</label>
+                                <input type="text" class="form-control mb-2" name="counter_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter Title One</label>
+                                <input type="text" class="form-control mb-2" name="counter_title_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter one</label>
+                                <input type="text" class="form-control mb-2" name="counter_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter Title One</label>
+                                <input type="text" class="form-control mb-2" name="counter_title_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter one</label>
+                                <input type="text" class="form-control mb-2" name="counter_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter Title One</label>
+                                <input type="text" class="form-control mb-2" name="counter_title_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter one</label>
+                                <input type="text" class="form-control mb-2" name="counter_one" >
+                            </div>
+                        </div>
+
+                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="" class="form-label mb-2">Counter Title One</label>
+                                <input type="text" class="form-control mb-2" name="counter_title_one" >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="mt-3">
+
+                    <button type="submit" class="btn btn-primary ">Update</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
